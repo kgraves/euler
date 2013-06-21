39b913a4396065748055e108a3f1fbc87d9e9700
kgraves <kgraves9@mail.csuchico.edu>
2013-06-21 00:29:44 -0700

    finishes p22.py

--- p22.py
+++ p22.py
@@ -1,5 +1,24 @@
+def calc_name_scores(names):
+    """ calculate the 'name scores' of a list of names """
+    limit = len(names)
+    base = ord('A')
+    total = 0
 
+    for i in xrange(limit):
+        score = 0
+
+        for char in names[i]:
+            score += ord(char) - base + 1
+
+        total += score * (i+1)
+
+    return total
 
 if __name__ == '__main__':
-    # parse input file
+    # parse input file and sort names
+    f = open('input/names.txt', 'r')
+    names = [ name.replace('"', '') for name in f.read().split(',') ]
+    names.sort()
+
+    print calc_name_scores(names)
 
