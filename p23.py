d009037229bb53214c5ba86f5616160ab6d0a126
kgraves <kgraves9@mail.csuchico.edu>
2013-06-21 21:58:40 -0700

    completes p23.py, pretty straight forward

--- p23.py
+++ p23.py
@@ -1,4 +1,11 @@
-def find_factors(n):
+# imports
+from math import ceil
+
+# GLOBALS
+MAX = 28123
+ABUNDANTS = {}
+
+def sum_factors(n):
     """ finds all factors of n and returns them in a list """
     factors = []
 
@@ -8,8 +15,30 @@ def find_factors(n):
         if n % i == 0:
             factors.append(i)
 
-    return factors
+    return sum(factors)
 
 if __name__ == '__main__':
-    # TODO add main logic here
-    pass
+    total = 0
+
+    for i in range(1, MAX):
+        # print "%d - %d" % (i, sum_factors(i))
+        s = sum_factors(i)
+        found = False
+
+        for key, val in ABUNDANTS.items():
+            if ABUNDANTS.has_key(i-key) and i == key + ABUNDANTS[i-key]:
+                found = True
+                break
+
+        # if i is not the sum of two abundants, add to total
+        if not found:
+            print i
+            total += i
+
+        # check if i is an abundant number
+        if s > i:
+            # add to abundants
+            ABUNDANTS[i] = i
+
+    print total
+
